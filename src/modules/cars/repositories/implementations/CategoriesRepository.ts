21f61b3babc84f232c0ea50d92efffa1668bddd4
Willames <wyllamesro@gmail.com>
2021-12-22 08:10:16 -0300

    correction of class names

---
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from "typeorm";
+import { Category } from "../../entities/Category";
+import {
+    ICategoriesRepository,
+    ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepository implements ICategoriesRepository {
+    private repository: Repository<Category>;
+
+    constructor() {
+        this.repository = getRepository(Category);
+    }
+
+   async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = this.repository.create({
+            name, 
+            description
+        })
+        
+        await this.repository.save(category)
+    }
+
+    async list(): Promise<Category[]> {
+        return await this.repository.find();
+    }
+    async findByName(name: string): Promise<Category> {
+        const cateory = await this.repository.findOne({name});
+        return cateory;
+    }
+}
+
+export { CategoriesRepository };
